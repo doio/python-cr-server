70e35e0eb510d6e8033d1a2cfc2267350ed66b75
Batch-Monger <jxnchess@gmail.com>
2017-05-27 16:48:37 -0400

    Squashed a bug or two
    Fixed a bug that caused the program to not allow more than one connection.

--- proxy-alpha.py
+++ proxy-alpha.py
@@ -18,30 +18,34 @@ def debug(debmessage):
         pass
 def randomBytes(n):
     return bytes(random.getrandbits(8) for i in range(n))
-def mockrcv():
+def mockrcv(mock):
     rdata = mock.recv(10086)
     if not rdata:
         return "nulldata"
     return rdata
 
-def serverlisten(client):
-    ndata=mockrcv()
+def serverlisten(mock, client):
+    ndata=mockrcv(mock)
     if(ndata=="nulldata"):
         print('[WARNING] Proxy ndata is empty')
         return False
     else:
         lmessage_id = int(str(struct.unpack('>H', ndata[:2]))[1:-2])
+        if (lmessage_id >= 30000 or lmessage_id < 10000):
+            lmessage_id = 'Unknown Message'
+        elif len(str(lmessage_id)) is not 5:
+            lmessage_id = 'Unknown Message'
         print('[OK] Server => Proxy', lmessage_id)
         response = ndata
-        cres = client.send(response)
-        if cres:
-            print('[OK] Proxy => Client', lmessage_id)
-            return True
+        try:
+            client.send(response)
+        except ConnectionAbortedError:
+            client.close()
+            debug('closed')
+            
+global gl_server_address
 gl_server_address = ('54.245.151.83', 9339) #gcrs.private-gamers.com
-mock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("[INFO] Proxy =><= Server Connecting to",gl_server_address[0],'on port',gl_server_address[1])
-mock.connect(gl_server_address)
-print('[INFO] Proxy =><= Server Connected')
+
 class ThreadedServer(object):
     def __init__(self, host, port):
         self.host = host
@@ -57,22 +61,45 @@ class ThreadedServer(object):
             client, address = self.sock.accept()
             print('[INFO] Client =>Proxy Client', address, 'connected')
             client.settimeout(60) #second timeout
-            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+            mock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("[INFO] Proxy =><= Server Connecting to",gl_server_address[0],'on port',gl_server_address[1])
+            mock.connect(gl_server_address)
+            print('[INFO] Proxy =><= Server Connected')
+            threading.Thread(target = self.listenToClient,args = (client,address,mock)).start()
 
-    def listenToClient(self, client, address):
+    def listenToClient(self, client, address, mock):
         while True:
-            data = client.recv(4098)
+            try:
+                data = client.recv(4098)
+            except:
+                debug('closed')
+                mock.close()
             try:
                 message_id = int(str(struct.unpack('>H', data[:2]))[1:-2])
             except:
                 message_id = 'Unknown Message'
+            try:
+                if (message_id >= 30000 or message_id < 10000):
+                    message_id = 'Unknown Message'
+            except:
+                message_id = 'Unknown Message'
+            try:
+                if len(str(message_id)) is not 5:
+                    message_id = 'Unknown Message'
+            except:
+                message_id = 'Unknown Message'
+
             print('[OK] Client => Proxy', message_id)
             fmessage = data
-            mock.sendall(fmessage)
+            try:
+                mock.sendall(fmessage)
+            except:
+                debug('done closing?')
+                break
             print('[OK] Proxy => Server', message_id)
             while 1:
                 debug('Listening to server')
-                r = serverlisten(client);
+                r = serverlisten(mock, client);
                 if r == False:
                     debug('No data from the server')
                     break
